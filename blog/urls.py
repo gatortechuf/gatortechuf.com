61fe3ac506546fac244221b69d81d76d3d0f823f
Ryan Sheppard <ryandsheppard95@gmail.com>
2016-06-19 19:36:33 -0500

    Basic commenting added

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.BlogPostView.as_view(), name='blogpostview'),
+    url(r'^add_comment/$', views.CommentView.as_view(), name='formview'),
 ]
