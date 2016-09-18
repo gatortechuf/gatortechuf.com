2f74280aed4020b3fca29f33de69c4974c755a2f
Ryan Sheppard <ryandsheppard95@gmail.com>
2016-09-18 00:01:55 -0400

    settings fix

--- gatortech/settings.py
+++ gatortech/settings.py
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # markdown settings
 # should you ever put an import here? probably not
