616ca4bca14ba9ba2ed58564ece4d60357367768
Ubuntu <ubuntu@ip-172-31-59-155.ec2.internal>
2017-01-02 23:49:46 +0000

    added new way to update events

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     url(r'^$', views.EventsView.as_view(), name='event_index'),
     url(r'^archive/$', views.OldEventsView.as_view(), name='event_archive'),
+    url(r'^update/$', views.UpdateEvents.as_view(), name='event_update'),
+    url(r'^updater/$', views.update_handler, name='event_updater'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,14 @@
+import json
+import urllib.parse
 from django.views import generic
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 from django.utils import timezone
+from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+from django.utils.decorators import method_decorator
 
 from .models import Event
 
@@ -31,3 +40,40 @@ class OldEventsView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(OldEventsView, self).get_context_data(**kwargs)
         return context
+
+class UpdateEvents(TemplateView):
+    template_name = 'events/update.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(UpdateEvents, self).dispatch(*args, **kwargs)
+
+def update_handler(request):
+    try:
+        #loaded = urllib.parse.unquote_plus(request.body.decode("utf-8"))
+        #loaded = json.loads(urllib.parse.unquote_plus(request.body.decode("utf-8")))
+        loaded = json.loads(request.POST['events'])
+    except Exception as e:
+        loaded = str(e)
+
+    for fb_event in loaded['events']:
+        if 'place' in fb_event and 'name' in fb_event['place']:
+            location = fb_event['place']['name']
+        else:
+            location = 'TBD'
+
+        if 'description' in fb_event:
+            description = fb_event['description']
+        else:
+            description = 'No Description'
+
+        event = Event.objects.update_or_create(id=fb_event['id'],
+                                                        slug=slugify(fb_event['name']),
+                                                        event_title=fb_event['name'],
+                                                        event_description=description,
+                                                        event_location_name=location,
+                                                        event_date=fb_event['start_time'],
+                                                        event_facebook_url='https://www.facebook.com/events/' + str(fb_event['id']))
+
+
+    return HttpResponse('done')
