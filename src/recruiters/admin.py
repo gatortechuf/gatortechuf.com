563334b949b233cae67031d83f0fa3813e6a78c7
bymadisonross <bymadisonross@gmail.com>
2017-09-06 08:48:14 -0400

    recruiter logos

--- src/recruiters/admin.py
+++ src/recruiters/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RecruiterLogo
+# class RecruiterAdmin(admin.ModelAdmin):
+admin.site.register(RecruiterLogo)
--- src/recruiters/models.py
+++ src/recruiters/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class RecruiterLogo(models.Model):
+    logo_name = models.CharField("Company Name", max_length = 128)
+    url_name = models.CharField("Company URL", max_length = 512)
+    picture_name = models.ImageField("Company Image", blank = False, upload_to = "logos")
+
+    def __str__(self):
+        return self.logo_name
