c1328a7d9f514bc5bb48619d910fb16ec138bc50
Ryan Sheppard <ryandsheppard95@gmail.com>
2017-09-26 12:41:51 -0400

    reverted to google calendar

--- src/events/admin.py
+++ src/events/admin.py
@@ -6,23 +6,12 @@ from django.db import models
 
 from pagedown.widgets import AdminPagedownWidget
 
-from .models import Event
+from .models import Calendar
 
-class EventAdmin(admin.ModelAdmin):
+class CalendarAdmin(admin.ModelAdmin):
     """
-    Events Admin
+    Calendar Admin
     """
-    list_display = ('event_title', 'event_location_name', 'event_date')
-    search_fields = ('event_title', 'event_location_name', 'event_description', 'event_tag')
-    prepopulated_fields = {'slug': ('event_title',)}
-    formfield_overrides = {
-        models.TextField: {'widget': AdminPagedownWidget}
-    }
-    fieldsets = [
-        ('Title and Type', {'fields': ('event_title', 'slug', 'event_facebook_url')}),
-        ('Location and Time', {'fields': ('event_location_name', 'event_date')}),
-        ('Description', {'fields': [('event_description')]})
-    ]
+    list_display = ('calendar_id',)
 
-
-admin.site.register(Event, EventAdmin)
+admin.site.register(Calendar, CalendarAdmin)
--- src/events/models.py
+++ src/events/models.py
@@ -3,17 +3,12 @@ Events Model
 """
 from django.db import models
 
-class Event(models.Model):
+class Calendar(models.Model):
     """
-    Events Model
+    Google Calendar Embed
     """
-    event_title = models.CharField('Title', max_length=128)
-    event_description = models.TextField('Description', max_length=4096, default='Description')
-    event_location_name = models.CharField('Location Name', max_length=256, default='Heavener Hall 150')
-    event_facebook_url = models.CharField('Facebook Event URL', max_length=256)
-    event_date = models.DateTimeField('Date', auto_now=False)
-    slug = models.SlugField('URL (Avoid Editing)', default=None, unique=True, max_length=128)
+    calendar_id = models.CharField('Calendar ID', max_length=1024)
 
     class Meta:
-        verbose_name = 'Event'
-        verbose_name_plural = 'Events'
+        verbose_name = 'Calendar'
+        verbose_name_plural = 'Calendars'
