a38b7446b8b4e4e47f51af851d4a818ad117803f
Ryan Sheppard <5157309+ryansheppard@users.noreply.github.com>
2017-10-25 17:42:18 -0400

    Dynamic homepage (#95)
    * homepage migrations + models
    
    * updated template
    
    * fixed bugs

--- src/home/models.py
+++ src/home/models.py
@@ -3,6 +3,39 @@ Homepage models
 """
 from django.db import models
 
+class HomePageHeader(models.Model):
+    """
+    Header underneath homepage logo
+    """
+    subtitle = models.TextField('Subtitle', max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subtitle
+
+class HomePageButton(models.Model):
+    """
+    Button row
+    """
+    icon = models.CharField('Font Awesome Icon', max_length=128)
+    button_text = models.CharField('Button Text', max_length=128)
+    button_link = models.CharField('Button Link', max_length=1024)
+
+    def __str__(self):
+        return self.button_text
+    
+class TextBlurb(models.Model):
+    """
+    Blocks that appear between semester modules and header buttons
+    """
+    icon = models.CharField('Font Awesome Icon', max_length=128)
+    blurb_title = models.CharField('Blurb Title', max_length=128)
+    blurb_text = models.CharField('Blurb Text', max_length=512)
+
+    def __str__(self):
+        return self.blurb_title
+
 class SemesterModule(models.Model):
     """
     SemesterModule contains the title, description, and icon for the
--- src/home/views.py
+++ src/home/views.py
@@ -3,12 +3,18 @@ Homepage Views
 """
 from django.views import generic
 
-from .models import SemesterModule
+from .models import HomePageHeader, HomePageButton, TextBlurb, SemesterModule
 
-class HomePageView(generic.ListView):
+class HomePageView(generic.TemplateView):
     """
     Homepage Index
     """
     template_name = 'home/landing_page.html'
-    model = SemesterModule
-    context_object_name = 'modules'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['header'] = HomePageHeader.objects.latest('updated_at')
+        context['buttons'] = HomePageButton.objects.all()
+        context['blurbs'] = TextBlurb.objects.all()
+        context['modules'] = SemesterModule.objects.all()
+        return context
