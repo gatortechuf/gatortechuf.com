b736b3f85342a5c2c00f6033ecbf77c9d18bf74f
Ryan Sheppard <ryandsheppard95@gmail.com>
2018-01-08 20:01:39 -0500

    resource page draft

--- src/resources/admin.py
+++ src/resources/admin.py
@@ -2,7 +2,9 @@
 Resources Admin
 """
 from django.contrib import admin
-from .models import Resource, ResourceFile
+from .models import Resource, ResourceFile, ResourceLink, ResourceImage
 
 admin.site.register(Resource)
 admin.site.register(ResourceFile)
+admin.site.register(ResourceLink)
+admin.site.register(ResourceImage)
--- src/resources/models.py
+++ src/resources/models.py
@@ -10,6 +10,9 @@ class Resource(models.Model):
     resource_name = models.CharField(max_length=256)
     resource_description = models.TextField("Description", max_length=2048)
 
+    def __str__(self):
+        return self.resource_name
+
 
 class ResourceFile(models.Model):
     """
@@ -17,4 +20,26 @@ class ResourceFile(models.Model):
     """
     resource_parent = models.ForeignKey(Resource)
     resource_file_name = models.CharField(max_length=256)
-    resource_file = models.FileField(max_length=1024)
+    resource_file = models.FileField(max_length=1024, upload_to='resources')
+
+    def __str__(self):
+        return self.resource_file_name
+
+class ResourceLink(models.Model):
+    """
+    Resource Link Model
+    """
+    resource_parent = models.ForeignKey(Resource)
+    resource_link = models.CharField(max_length=1024)
+    resource_link_name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.resource_link_name
+
+
+class ResourceImage(models.Model):
+    """
+    Resource Header Image
+    """
+    resource_parent = models.ForeignKey(Resource)
+    resource_image = models.ImageField(upload_to='resource_images')
