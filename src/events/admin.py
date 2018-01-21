47a951e9be938c4ca3eb5e7c98932c300a6c31a6
Ryan Sheppard <ryandsheppard95@gmail.com>
2018-01-21 11:45:45 -0500

    updated admin

--- src/events/admin.py
+++ src/events/admin.py
@@ -4,8 +4,6 @@ Events Admin
 from django.contrib import admin
 from django.db import models
 
-from pagedown.widgets import AdminPagedownWidget
-
 from .models import Calendar
 
 class CalendarAdmin(admin.ModelAdmin):
