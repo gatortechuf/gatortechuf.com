46d0e61491baca3587f6f638ad3dc79bf1614bd9
Ryan Sheppard <5157309+ryansheppard@users.noreply.github.com>
2018-01-27 14:46:10 -0500

    filter by users for checkin (#110)

--- src/attendance/views.py
+++ src/attendance/views.py
@@ -13,7 +13,7 @@ def index(request):
 def checkin(request):
     current_time = timezone.now()
     if (23 <= current_time.time().hour or 0 <= current_time.time().hour <= 1) and current_time.date().weekday() == 1:
-        recent_check_in = CheckIn.objects.all().filter(checked_in_on__gt=timezone.now() - timedelta(hours=4))
+        recent_check_in = CheckIn.objects.all().filter(user=request.user, checked_in_on__gt=timezone.now() - timedelta(hours=4))
         if recent_check_in:
             context = {"message": "You have already checked in"}
         else:
